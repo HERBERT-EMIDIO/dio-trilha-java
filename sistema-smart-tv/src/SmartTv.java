1d0cac97d5c4afdbee573b14a8fc4e0c315c0490
Herbert-Emidio <herbertemidio2017@gmail.com>
2024-08-01 15:22:24 -0300

    comentarios - metodos

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,51 +1,51 @@
 public class SmartTv {
 
-    //=================== Atributos ====================
+    // =================== Atributos ====================
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
 
-    //================= Métodos sem parâmetros ==============
-    public void ligar(){
+    // ================= Métodos sem parâmetros ==============
+    public void ligar() {
         ligada = true;
     }
 
-    public void desligar(){
+    public void desligar() {
         ligada = false;
     }
 
     // Volume
-    public void aumentarVolumeBotaoSetaParaDireita(){
+    public void aumentarVolumeBotaoSetaParaDireita() {
         volume++;
     }
 
-    public void diminuirVolumeBotaoSetaParaDireita(){
+    public void diminuirVolumeBotaoSetaParaDireita() {
         volume--;
     }
 
     // Canal
-    public void mudarDeCanalBotaoSetaParaCima(){
+    public void mudarDeCanalBotaoSetaParaCima() {
         canal++;
     }
-    public void mudarDeCanalBotaoSetaParaBaixo(){
+
+    public void mudarDeCanalBotaoSetaParaBaixo() {
         canal--;
     }
-    
-    //=========== Métodos com um parâmetro ===========
+
+    // =========== Métodos com um parâmetro ===========
 
     // Volume
-    public int aumentarVolume(int value){
+    public int aumentarVolume(int value) {
         return volume = volume + value;
     }
 
-    public int diminuirVolume(int value){
-        return volume-=value;
+    public int diminuirVolume(int value) {
+        return volume -= value;
     }
 
     // Canal
-    public int mudarCanal(int novoCanal){
+    public int mudarCanal(int novoCanal) {
         return canal = novoCanal;
     }
-
 
 }
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,38 +1,38 @@
 public class Usuario {
     public static void main(String[] args) throws Exception {
 
-        // Criando a SmartTV:    
+        // Criando a SmartTV:
         SmartTv minhaSmartTv = new SmartTv();
 
-        System.out.println("Ligada: "+ minhaSmartTv.ligada);
-        System.out.println("Volume: "+ minhaSmartTv.volume);
-        System.out.println("Canal: "+ minhaSmartTv.canal);
+        System.out.println("Ligada: " + minhaSmartTv.ligada);
+        System.out.println("Volume: " + minhaSmartTv.volume);
+        System.out.println("Canal: " + minhaSmartTv.canal);
         System.out.println();
-        
+
         // Chamada do método para ligar:
         minhaSmartTv.ligar();
         System.out.println("Ligada: " + minhaSmartTv.ligada);
         minhaSmartTv.desligar();
         System.out.println("Ligada: " + minhaSmartTv.ligada);
         System.out.println();
-        
+
         // Camada do método para aumentar o Volume dizendo o parâmetro
         minhaSmartTv.aumentarVolume(20);
-        System.out.println("Volume Aumentado: "+ minhaSmartTv.volume);
-        
+        System.out.println("Volume Aumentado: " + minhaSmartTv.volume);
+
         minhaSmartTv.diminuirVolume(45);
-        System.out.println("Volume diminuido: "+ minhaSmartTv.volume);
+        System.out.println("Volume diminuido: " + minhaSmartTv.volume);
         System.out.println();
-        
+
         // Mudar de canal
         minhaSmartTv.mudarCanal(13);
         System.out.println();
-        
+
         minhaSmartTv.mudarDeCanalBotaoSetaParaCima();
-        System.out.println("Canal: "+ minhaSmartTv.canal);
+        System.out.println("Canal: " + minhaSmartTv.canal);
         System.out.println();
         minhaSmartTv.mudarDeCanalBotaoSetaParaBaixo();
-        System.out.println("Canal: "+ minhaSmartTv.canal);
+        System.out.println("Canal: " + minhaSmartTv.canal);
         System.out.println();
 
     }
