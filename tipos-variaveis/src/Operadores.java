9bf0b3165ec261a532ff77e66f4c56c363546997
Herbert-Emidio <herbertemidio2017@gmail.com>
2024-08-01 10:09:44 -0300

    operadoreas

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,10 +1,24 @@
 public class Operadores {
 
     public static void main(String[]args){
-
+/* 
         String nomeCompleto = "Herbert Emidio";
-        System.out.print(nomeCompleto);
+        int numero = 2;
+        System.out.println(nomeCompleto);
+        System.out.println(numero);
+        System.out.println(++ numero);
+        System.out.println(numero ++);
+*/
         
+    // Operador ternário
+
+        int a, b;
+        a = 10;
+        b = 20;
+
+        String resultado = a==b ? "Verdade": "Falso";
+        System.out.println(resultado);
+
 
     }
     
