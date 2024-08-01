632bcd2a0e4191a7a906f9c71883cabcfc3d0d5c
Herbert-Emidio <herbertemidio2017@gmail.com>
2024-07-31 23:03:18 -0300

    Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+
+    public static void main(String[]args){
+
+        String nomeCompleto = "Herbert Emidio";
+        System.out.print(nomeCompleto);
+        
+
+    }
+    
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        double salarioMinimo = 2500.33;
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;//cast()
+        
+        // Declarar um valor que nunca poderá mudar com 'final' + nome em MAIUSCULO
+        final int CPF = 3;
+        final double VALOR_DE_PI = 13.14;
+
+        // TEXTOS + Strings
+
+        String nome = "Herbert Felipe";
     }
 }
 
