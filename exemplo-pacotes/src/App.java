bf58e760076b8d0c6b8a83323b78d78635c66826
Herbert-Emidio <herbertemidio2017@gmail.com>
2024-08-06 08:42:50 -0300

    add readme

---
+++ exemplo-pacotes/src/App.java
@@ -0,0 +1,9 @@
+import com.mastersoft.clinical.model.Exame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+    
+    }
+}
