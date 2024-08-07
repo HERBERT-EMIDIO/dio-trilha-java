dfd46612ae39079b5be44bdbbc6ab8a169481b40
Herbert-Emidio <herbertemidio2017@gmail.com>
2024-08-07 16:49:51 -0300

    construtores

--- getter-setters/src/escola/Pessoa.java
+++ getter-setters/src/escola/Pessoa.java
@@ -1,35 +1,30 @@
 package escola;
 
-
-
 public class Pessoa {
 
     private String nome;
     private int idade;
 
-    
-    // Método construtor : se criar uma pessoa de cara temos que informar os parametros
-    public Pessoa(String nome, int idade){
+    // Método construtor : se criar uma pessoa de cara temos que informar os
+    // parametros
+    public Pessoa(String nome, int idade) {
         this.nome = nome;
         this.idade = idade;
     }
 
-
-
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
 
-    public void setNome(String newNome){
+    public void setNome(String newNome) {
         nome = newNome;
     }
 
-    public int getIdade(){
-        return idade; 
+    public int getIdade() {
+        return idade;
     }
-    
 
-    public void setIdade(int newIdade){
+    public void setIdade(int newIdade) {
         idade = newIdade;
     }
 }
